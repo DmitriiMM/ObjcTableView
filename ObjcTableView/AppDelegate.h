e94af6afdbf00ef99cda3a20604f21a5a950bcf5
Dmitrii Martynov <martinovit72@gmail.com>
2023-08-08 14:49:01 +0500

    Initial Commit

---
+++ ObjcTableView/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ObjcTableView
+//
+//  Created by Дмитрий Мартынов on 08.08.2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ObjcTableView/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ObjcTableView
+//
+//  Created by Дмитрий Мартынов on 08.08.2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
